28efb7695443ac9019180e05af4ca33064af8e1a
Noah Loomans <noahloomans@gmail.com>
2016-02-26 21:15:37 +0100

    Fixed not adding group before add user to group and added status updates

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+echo "copying nbc..."
+cp ./nbc /usr/local/bin
+echo "copying nxc..."
+cp ./nxc /usr/bin
+echo "copying rules..."
 cp ./70-nxt.rules /etc/udev/rules.d
+echo "creating nxt group..."
+sudo -u $SUDO_USER groupadd nxt
+sudo -u $SUDO_USER echo "adding `whoami` to nxt group..."
 sudo -u $SUDO_USER usermod -a -G nxt $(whoami)
-cp ./nxc /usr/bin/nxc
-sudo -u $SUDO_USER echo "please run \`sudo visudo\` and add \"`whoami` ALL = NOPASSWD: /usr/local/bin/nbc\""
+sudo -u $SUDO_USER echo "to finish the installation, please run \`sudo visudo\` and add \"`whoami` ALL = NOPASSWD: /usr/local/bin/nbc\""
