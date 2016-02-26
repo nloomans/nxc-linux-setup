7369d3ca1b7a2e7630e7a646da0d7d580cab36b8
Noah Loomans <noahloomans@gmail.com>
2016-02-26 17:57:53 +0100

    Fixed wrong user bug

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cp ./70-nxt.rules /etc/udev/rules.d
-usermod -a -G nxt $(whoami)
+sudo -u $SUDO_USER usermod -a -G nxt $(whoami)
 cp ./nxc /usr/bin/nxc
-echo "please run \`sudo visudo\` and add \"`whoami` ALL = NOPASSWD: /usr/local/bin/nbc\""
+sudo -u $SUDO_USER echo "please run \`sudo visudo\` and add \"`whoami` ALL = NOPASSWD: /usr/local/bin/nbc\""
