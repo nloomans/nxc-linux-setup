e56ddf8e69d15b84b72f07df26f8859354b43c78
Noah Loomans <noahloomans@gmail.com>
2016-02-27 21:52:59 +0100

    Removed unnesesary visudo

--- install.sh
+++ install.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
+if [ "$SUDO_USER" != "" ]; then
+  user="$SUDO_USER"
+else
+  user=`whoami`
+fi
+  
+if [ "$(id -u)" -ne 0 ]; then
+  echo "This script has to be run as root (try \`sudo ./install.sh\`)"
+  exit 1
+fi
+
 echo "copying nbc..."
 cp ./nbc /usr/local/bin
 echo "copying nxc..."
@@ -6,9 +17,7 @@ cp ./nxc /usr/bin
 echo "copying rules..."
 cp ./70-nxt.rules /etc/udev/rules.d
 echo "creating nxt group..."
-sudo -u $SUDO_USER groupadd nxt
-sudo -u $SUDO_USER echo "adding `whoami` to nxt group..."
-sudo -u $SUDO_USER usermod -a -G nxt $(whoami)
-sudo -u $SUDO_USER echo "to finish the installation, please run \`sudo"\
-"visudo\` and add \"`whoami` ALL = NOPASSWD: /usr/local/bin/nbc\" to the end"\
-"of the file."
+groupadd nxt
+echo "adding $user to nxt group..."
+usermod -a -G nxt $user
+echo "installation sucsesful!"
